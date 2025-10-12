887bbe54dea6ecbdc4e25e341f904b0cc8baf502
DemonDev0666 <DemonDev0666@gmail.com>
2025-10-11 23:07:34 -0400

    Initial commit

---
+++ src/main/java/com/demondev/infernoRiftsModeration/InfernoRiftsModeration.java
@@ -0,0 +1,17 @@
+package com.demondev.infernoRiftsModeration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InfernoRiftsModeration extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
