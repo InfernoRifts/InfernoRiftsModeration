c551d93f1fbde1f3626e4c48dfb12d08e1722c32
DemonDev0666 <DemonDev0666@gmail.com>
2025-11-06 12:28:23 -0500

    Created BanMenu

--- src/main/java/com/demondev/infernoRiftsModeration/InfernoRiftsModeration.java
+++ src/main/java/com/demondev/infernoRiftsModeration/InfernoRiftsModeration.java
@@ -1,15 +1,18 @@
 package com.demondev.infernoRiftsModeration;
 
-import com.demondev.infernoRiftsModeration.commands.ModToolsCommand;
-import com.demondev.infernoRiftsModeration.commands.ModToolsTabComplete;
+import com.demondev.infernoRiftsModeration.commands.BanGUICommand;
+import com.demondev.infernoRiftsModeration.listeners.BanInventoryListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InfernoRiftsModeration extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getCommand("modtools").setExecutor(new ModToolsCommand());
-        this.getCommand("modtools").setTabCompleter(new ModToolsTabComplete());
+
+        getCommand("bangui").setExecutor(new BanGUICommand());
+
+        getServer().getPluginManager().registerEvents(new BanInventoryListener(), this);
+
     }
 
     @Override
---
+++ src/main/java/com/demondev/infernoRiftsModeration/listeners/BanInventoryListener.java
@@ -0,0 +1,42 @@
+package com.demondev.infernoRiftsModeration.listeners;
+
+import com.demondev.infernoRiftsModeration.utils.BanMenuUtils;
+import org.bukkit.BanList;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class BanInventoryListener implements Listener {
+
+    @EventHandler
+    public void onMenuClick(InventoryClickEvent e) {
+
+        Player p = (Player) e.getWhoClicked();
+
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE + "Player List")){
+
+            if (e.getCurrentItem().getType() == Material.PLAYER_HEAD){
+
+                Player whoToBan = p.getServer().getPlayerExact(ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName()));
+
+                BanMenuUtils.openConfirmBanMenu(p, whoToBan);
+
+            }
+        }else if (e.getView().getTitle().equalsIgnoreCase(ChatColor.RED + "Are You sure want to ban this player?")){
+
+            if (e.getCurrentItem().getType() == Material.BARRIER){
+                BanMenuUtils.openBanMenu(p);
+          }else if(e.getCurrentItem().getType() == Material.LIME_CONCRETE){
+                String name = ChatColor.stripColor(e.getClickedInventory().getItem(4).getItemMeta().getDisplayName());
+                p.getServer().getBanList(BanList.Type.PROFILE).addBan(name, "Admin decided", null, null);
+                p.sendMessage(ChatColor.GREEN + "Banned Player");
+            }
+        }
+        e.setCancelled(true);
+
+    }
+
+}
